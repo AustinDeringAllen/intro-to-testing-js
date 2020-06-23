acb6d2b859e7047d05761797e24434e7aed3baca
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-06-23 14:52:42 -0500

    added third test and solutuion to intro-to-testing

--- tests.js
+++ tests.js
@@ -21,6 +21,9 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return "Hello, Jane!"', function() {
+       expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
 })
 
 // describe('isEven',function() {
